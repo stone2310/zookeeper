c1def235d3e06bb7a1951a56a684f759a9abde34
yuhuz-company <yuhuz-company@sina.com>
2016-06-17 19:00:11 +0800

    commit

--- zkClient/src/main/java/com/zk/dev/curator/CuratorClientTestMe.java
+++ zkClient/src/main/java/com/zk/dev/curator/CuratorClientTestMe.java
@@ -8,6 +8,13 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.BackgroundCallback;
 import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache.StartMode;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent.Type;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
@@ -20,7 +27,7 @@ public class CuratorClientTestMe {
 	
 	public CuratorClientTestMe(){
 		RetryPolicy retryPolicy=new ExponentialBackoffRetry(1000,3);
-		client=CuratorFrameworkFactory.builder().connectString("192.168.3.71:2181")
+		client=CuratorFrameworkFactory.builder().connectString("192.168.145.128:2181,192.168.145.128:2182,192.168.145.128:2183")
 				.sessionTimeoutMs(10000).retryPolicy(retryPolicy).connectionTimeoutMs(15000).namespace("base").build();
 		client.start();
 		
@@ -28,11 +35,13 @@ public class CuratorClientTestMe {
 	//创建节点
 	public void creatNode(String path,String data) throws Exception{
 		client.create().withMode(CreateMode.PERSISTENT).withACL(Ids.OPEN_ACL_UNSAFE).forPath(path, data.getBytes());
+		System.out.println("创建"+path+"成功");
 	}
 	
 	//修改节点
 	public void setData(String path,int version,String data)throws Exception{
 		client.setData().withVersion(version).forPath(path, data.getBytes());
+		System.out.println("修改"+path+"成功");
 	}
 	
 	//删除节点
@@ -43,10 +52,10 @@ public class CuratorClientTestMe {
 			@Override
 			public void processResult(CuratorFramework client, CuratorEvent event) throws Exception {
 				try {
-					System.out.println("event.getStat():"+event.getStat()+" thread:"+Thread.currentThread().getName());
-					System.out.println("event.getType():"+event.getType());
-					System.out.println("event.getData():"+(event.getData()!=null?new String(event.getData()):""));
-					System.out.println("event.getResultCode():"+event.getResultCode());
+					System.out.println("删除event.getStat():"+event.getStat()+" thread:"+Thread.currentThread().getName());
+					System.out.println("删除event.getType():"+event.getType());
+					System.out.println("删除event.getData():"+(event.getData()!=null?new String(event.getData()):""));
+					System.out.println("删除event.getResultCode():"+event.getResultCode());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -84,6 +93,64 @@ public class CuratorClientTestMe {
 		System.out.println("stat:"+stat);
 	}
 	
+	//给节点添加watch
+	//创建节点、删除节点、修改节点
+	public void addWatch(String path)throws Exception{
+		final NodeCache nc=new NodeCache(client, path);
+		nc.start(true);
+		nc.getListenable().addListener(new NodeCacheListener() {
+			
+			@Override
+			public void nodeChanged() throws Exception {
+				try {
+					if(nc.getCurrentData()!=null){
+						System.out.println("监听data:"+(nc.getCurrentData())!=null?(nc.getCurrentData().getData()):"");
+						System.out.println("监听path:"+nc.getCurrentData().getPath());
+						System.out.println("监听sata:"+nc.getCurrentData().getStat());
+					}else{
+						System.out.println("监听到删除操作");
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	//子节点新增、变更、删除
+	public void addChildWatch(String path)throws Exception{
+		PathChildrenCache pcc=new PathChildrenCache(client, path, true);
+		pcc.start(StartMode.POST_INITIALIZED_EVENT);
+		System.out.println(pcc.getCurrentData().size());
+		pcc.getListenable().addListener(new PathChildrenCacheListener() {
+
+			@Override
+			public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+				switch(event.getType()){
+				case INITIALIZED:
+					System.out.println("INITIALIZED"+Type.INITIALIZED);
+					System.out.println("节点初始化完成");
+					break;
+				case CHILD_ADDED:
+					System.out.println("CHILD_ADDED"+Type.CHILD_ADDED);
+					System.out.println("新增子节点："+event.getData().getPath());
+					break;
+				case CHILD_UPDATED:
+					System.out.println("CHILD_ADDED"+Type.CHILD_UPDATED);
+					System.out.println("修改子节点："+event.getData().getPath());
+					break;
+				case CHILD_REMOVED:
+					System.out.println("CHILD_ADDED"+Type.CHILD_REMOVED);
+					System.out.println("删除子节点："+event.getData().getPath());
+					break;
+				default:
+					break;
+				}
+			}
+			
+		});
+	}
+	
 	public void closeClient(){
 		if(client!=null){
 			client.close();
@@ -97,11 +164,20 @@ public class CuratorClientTestMe {
 	public static void main(String[] args) {
 		CuratorClientTestMe cctm=new CuratorClientTestMe();
 		try {
-//			cctm.creatNode("/curator/test3", "curator");
+			cctm.addChildWatch("/curator");
+			Thread.sleep(2000);
+			cctm.creatNode("/curator/test2", "test2");
+			Thread.sleep(2000);
+			cctm.setData("/curator/test2", 0, "test2new");
+			Thread.sleep(2000);
+			cctm.deleteNode("/curator/test2", 1);
+			
+			
+			
 			
 //			cctm.deleteNode("/curator", 0);
 //			cctm.setData("/curator",0,"newdata");
-			cctm.getChildren("/curator");
+//			cctm.getChildren("/curator");
 			Thread.sleep(30000);
 		} catch (Exception e) {
 			e.printStackTrace();
